717aa588b2789b927e6c4b815c0567939a0fc297
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-08 22:47:23 -0300

    Finalizando o exercício com eventos com targets e listeners.

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -26,7 +26,41 @@ botaoAddTarefa.addEventListener("click", (evt)=>{
     //Inserindo o elemento li criando em cativeiro.
     listaTarefasUL.appendChild(li);
 
+    //Criando um botão para excluir as tarefas.
+    let botaoExcluir = document.createElement("button");
+
+    //Adicionando texto ao botão de exclusão de tarefa
+    botaoExcluir.textContent = " x ";
+
+    //Adicionando o botão de excluir tarefas ao elemento li.
+    li.appendChild(botaoExcluir);
+
+    //Adicionando um listener de evento para o botão excluir
+    botaoExcluir.addEventListener("click",(evt)=>{
+
+        let conteudoDeTextoDoLiArray = evt.target.parentNode.textContent.split(" ");
+        let indice = listaTarefasArray.indexOf(conteudoDeTextoDoLiArray[0]);
+        
+        listaTarefasArray.forEach(tarefa=>{
+
+            if(conteudoDeTextoDoLiArray[0] == tarefa){
+                listaTarefasArray.splice(indice,1);
+            }
+        });
+
+        evt.target.parentNode.remove();
+    });
+
     inputTarefa.value = ""
     console.log(listaTarefasArray);
 
 });
+
+
+
+// let frase = "Eu amo, o meu, lindo PROFESSOR, ele, sempre, tenta, dar o seu melhor!";
+// console.log(frase);
+// let arrayGerado = frase.split("a");
+// console.log(arrayGerado);
+
+
